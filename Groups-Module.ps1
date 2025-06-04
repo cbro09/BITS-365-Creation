c814757da89d081371d5d957a2e6ff24f053430b
Callum <cbrodie09@outlook.com>
2025-06-04 09:26:18 +0100

    remove "Function new-Grouptenants"

--- Groups-Module.ps1
+++ Groups-Module.ps1
@@ -7,7 +7,7 @@ $DefaultGroups = @{
     License = @("BusinessBasic", "BusinessStandard", "BusinessPremium", "ExchangeOnline1", "ExchangeOnline2")
 }
 
-function New-TenantGroups {
+
     Write-LogMessage -Message "Starting group creation process..." -Type Info
     Import-RequiredGraphModules
     
@@ -111,4 +111,3 @@ function New-TenantGroups {
         Write-LogMessage -Message "Error in group creation process - $($_.Exception.Message)" -Type Error
         return $false
     }
-}
